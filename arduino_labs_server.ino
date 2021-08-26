3b0910693539acb0a0e7d1afd4fd2b5bcc177594
Ilya <ilyasmirkin200137@gmail.com>
2021-08-26 14:00:54 +0000

    v0.4.4 upgrade

--- arduino_labs_server.ino
+++ arduino_labs_server.ino
@@ -1,16 +1,16 @@
 //comment line below to disable debug mode
 #define DEBUG_MODE
 
-#define SOFTVERSION "v0.4.3-debug"
+#define SOFTVERSION "v0.4.4-debug"
 
-//    ANALOG magnetic includes    //
+//    ANALOG magnet includes    //
 
 uint8_t magnetAnalogIn = A0;
-uint32_t sensval = 0;
-uint32_t bval = 0;
+uint16_t sensval = 0;
+int16_t bval = 0;
 const float k = 100.0 / (3.3 / 2);
 const float kfx = (3.3 / 2) / 512 * k;
-float outval = 0;
+int32_t outval = 0;
 uint32_t del_magnet = 1000;
 
 //      BLE includes          //
@@ -22,7 +22,7 @@ BLECharacteristic TCS34725_NOTIFY_CHR_UID("48606B19-820D-48D0-91B8-DF6A4F8DCBD4"
 BLECharacteristic DS18B20_SEND_CHR_UID("09EB425D-8627-4FCB-AEA8-638BCF3F73F7", BLERead | BLEWrite, 5, true);
 BLECharacteristic DS18B20_NOTIFY_CHR_UID("8103F9B3-C91E-47CD-8634-4B7D8F4D018D", BLERead | BLENotify, 5, true);
 BLECharacteristic MAGNET_SEND_CHR_UID("3B75281E-00A0-4424-84C5-4C549CC1AE82", BLERead | BLEWrite, 5, true);
-BLECharacteristic MAGNET_NOTIFY_CHR_UID("EF8A1B0B-1005-4DAD-B49D-75F84488E52C", BLERead | BLENotify, 5, true);
+BLECharacteristic MAGNET_NOTIFY_CHR_UID("EF8A1B0B-1005-4DAD-B49D-75F84488E52C", BLERead | BLENotify, 6, false);
 BLECharacteristic MAX31855K_SEND_CHR_UID("2DAF3C9C-CABA-461C-9FBC-1839D6F4E5B9", BLERead | BLEWrite, 5, true);
 BLECharacteristic MAX31855K_NOTIFY_CHR_UID("F449E6B7-FE1E-45FF-AEBB-DCFC914DEB42", BLERead | BLENotify, 5, true);
 uint8_t ds18b20_n = 0, tcs34725_n = 0, magnet_n = 0, max31855_n = 0;
@@ -185,7 +185,7 @@ void tcs34725() {
     Serial.println(s[1], HEX);
     Serial.println(s[2], HEX);
 #endif
-    TCS34725_NOTIFY_CHR_UID.writeValue(s, 17);
+    TCS34725_NOTIFY_CHR_UID.writeValue(s, sizeof(s));
     ThisThread::sleep_for(del_tcs34725);
   }
 }
@@ -214,7 +214,7 @@ void bat3u() {
 #endif
 }
 
-//routine for analog magnetic sensor -----------------------------------------
+//routine for analog magnet sensor -------------------------------------------
 
 void magnet() {
   pinMode(magnetAnalogIn, INPUT);
@@ -224,8 +224,8 @@ void magnet() {
     outval = bval * kfx;
     outval *= -1;
     uint8_t s[6];
-    s[0] = 0x00; 
-    s[1] = 0x01;
+    s[0] = 0x00;
+    s[1] = 0x00;
     memcpy(&s[2], &outval, sizeof(outval));
 #ifdef DEBUG_MODE
     Serial.print("Sensor raw value: ");
@@ -233,7 +233,7 @@ void magnet() {
     Serial.print("\tMagnet value: ");
     Serial.println(outval);
 #endif
-    MAGNET_NOTIFY_CHR_UID.writeValue(s, 6);
+    MAGNET_NOTIFY_CHR_UID.writeValue(s, sizeof(s));
     ThisThread::sleep_for(del_magnet);
   }
 }
@@ -242,21 +242,21 @@ void magnet() {
 
 void BLEconnectHandler(BLEDevice central) {
 #ifdef DEBUG_MODE
-  Serial.print("Connected central: ");
+  Serial.print("[DEBUG]\tConnected central: ");
   Serial.println(central.address());
 #endif
 }
 
 void BLEdisconnectHandler(BLEDevice central) {
 #ifdef DEBUG_MODE
-  Serial.print("Disconnected central: ");
+  Serial.print("[DEBUG]\tDisconnected central: ");
   Serial.println(central.address());
 #endif
 }
 
 void BLEwriteDS18B20Handler(BLEDevice central, BLECharacteristic characteristic) {
 #ifdef DEBUG_MODE
-  Serial.println("Write event");
+  Serial.println("[DEBUG]\tWrite event");
 #endif
   uint8_t n_before = ds18b20_n;     //remembering last _n value
   ds18b20_n = (uint8_t)DS18B20_SEND_CHR_UID.value()[0];   //to prevent exceptions
@@ -269,16 +269,18 @@ void BLEwriteDS18B20Handler(BLEDevice central, BLECharacteristic characteristic)
   }
 #ifdef DEBUG_MODE //code below isn't necessary, but it's good for debugging
   else if (!ds18b20_n && !n_before) { //we can't stop thread, if it isn't running
-    Serial.println("!Illegal event: stopping non-existing thread of ds18b20!");
-  } else {  //we can't run already running thread
-    Serial.println("!Illegal event: launching another thread of ds18b20!");
+    Serial.println("[ERROR]\tIllegal event: stopping non-existing thread of ds18b20!");
+  } else if (ds18b20 && n_before) { //we can't run already running thread
+    Serial.println("[ERROR]\tIllegal event: launching another thread of ds18b20!");
   }
+  Serial.print("[DEBUG]\tdelay_ds18b20 = ");
+  Serial.println(del_ds18b20);
 #endif
 }
 
 void BLEwriteTCS34725Handler(BLEDevice central, BLECharacteristic characteristic) {
 #ifdef DEBUG_MODE
-  Serial.println("Write event");
+  Serial.println("[DEBUG]\tWrite event");
 #endif
   uint8_t n_before = tcs34725_n;
   uint8_t s[5];
@@ -296,17 +298,18 @@ void BLEwriteTCS34725Handler(BLEDevice central, BLECharacteristic characteristic
   }
 #ifdef DEBUG_MODE
   else if (!tcs34725_n && !n_before) {
-    Serial.println("!Illegal event: stopping non-existing thread of tcs34725!");
-  } else {
-    Serial.println("!Illegal event: launching another thread of tcs34725!");
+    Serial.println("[ERROR]\tIllegal event: stopping non-existing thread of tcs34725!");
+  } else if (tcs34725 && n_before) {
+    Serial.println("[ERROR]\tIllegal event: launching another thread of tcs34725!");
   }
-  Serial.println(del_tcs34725, DEC);
+  Serial.print("[DEBUG]\tdelay_tcs34725 = ");
+  Serial.println(del_tcs34725);
 #endif
 }
 
 void BLEwriteMAGNETHandler(BLEDevice central, BLECharacteristic characteristic) {
 #ifdef DEBUG_MODE
-  Serial.println("Write event");
+  Serial.println("[DEBUG]\tWrite event");
 #endif
   uint8_t n_before = magnet_n;
   magnet_n = (uint8_t)MAGNET_SEND_CHR_UID.value()[0];
@@ -318,7 +321,7 @@ void BLEwriteMAGNETHandler(BLEDevice central, BLECharacteristic characteristic) 
       A1Thread = new Thread;
       A1Thread->start(magnet);
     }
-  } else if (!magnet_n && n_before && magnet_conn) {
+  } else if (!magnet_n && n_before) {
     if (magnetAnalogIn == A0) {
       A0Thread->terminate();
       delete A0Thread;
@@ -330,12 +333,14 @@ void BLEwriteMAGNETHandler(BLEDevice central, BLECharacteristic characteristic) 
     MAGNET_SEND_CHR_UID.setValue("");
     magnet_n = 0;
 #ifdef DEBUG_MODE
-    Serial.println("!Illegal event: sensor is not connected!");
+    Serial.println("[ERROR]\tIllegal event: sensor is not connected!");
   } else if (!magnet_n && !n_before) {
-    Serial.println("!Illegal event: stopping non-existing thread of magnet!");
+    Serial.println("[ERROR]\tIllegal event: stopping non-existing thread of magnet!");
   } else if (magnet_n && n_before) {
-    Serial.println("!Illegal event: launching another thread of magnet!");
+    Serial.println("[ERROR]\tIllegal event: launching another thread of magnet!");
   }
+  Serial.print("[DEBUG]\tdelay_magnet = ");
+  Serial.println(del_magnet);
 #else
   }
 #endif
@@ -343,7 +348,7 @@ void BLEwriteMAGNETHandler(BLEDevice central, BLECharacteristic characteristic) 
 
 void BLEwriteMAX31855Handler(BLEDevice central, BLECharacteristic characteristic) {
 #ifdef DEBUG_MODE
-  Serial.println("Write event");
+  Serial.println("[DEBUG]\tWrite event");
 #endif
   uint8_t n_before = max31855_n;
   uint8_t s[5];
@@ -361,10 +366,11 @@ void BLEwriteMAX31855Handler(BLEDevice central, BLECharacteristic characteristic
   }
 #ifdef DEBUG_MODE
   else if (!max31855_n && !n_before) {
-    Serial.println("!Illegal event: stopping non-existing thread of tcs34725!");
+    Serial.println("[ERROR]\tIllegal event: stopping non-existing thread of tcs34725!");
   } else {
-    Serial.println("!Illegal event: launching another thread of tcs34725!");
+    Serial.println("[ERROR]\tIllegal event: launching another thread of tcs34725!");
   }
+  Serial.print("[DEBUG]\tdelay_max31855 = ");
   Serial.println(del_max31855, DEC);
 #endif
 }
@@ -394,14 +400,14 @@ void setup() {
   Serial.begin(9600);
   while (!Serial) delay(1);
   Serial.println();
-  Serial.print("Testing software ");
+  Serial.print("Multisensor software ");
   Serial.print(SOFTVERSION);
   Serial.println(" init... ");
-  Serial.print("Starting BLE... ");
+  Serial.print("[DEBUG]\tStarting BLE... ");
 #endif
   if (BLE.begin()) {
     Serial.println("DONE.");
-    Serial.print("Loading BLE name and service... ");
+    Serial.print("[DEBUG]\tLoading BLE name and service... ");
     BLE.setLocalName("LAB33");
     BLE.setAdvertisedService(labService);
     //here we should add characteristics to our service
